d63953472dc756fec7e5e74ed37aed450520efd9
zhiruchen <zhiruchen1992@foxmail.com>
2016-11-06 23:31:43 +0800

    add server1

---
+++ ch1/server1.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/t", handler1)
+	log.Fatal(http.ListenAndServe("localhost:9999", nil))
+}
+
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+
+func handler1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+
+	fmt.Fprintf(w, "Host: %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAdrres: %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}

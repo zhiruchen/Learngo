849c26467787ccd5aaf691320567ffc744a95f01
zhiruchen <zhiruchen1992@foxmail.com>
2016-12-17 16:20:08 +0800

    go struct

--- go_pointer.go
+++ go_pointer.go
@@ -10,9 +10,19 @@ func main() {
     pointer_test(&x)
     pointer_test(ptr_of_int)
 
+    x1, y1 := 1, 2
+    swap_number(&x1, &y1)
+    fmt.Println(x1, y1)
+
 }
 
 func pointer_test(xPtr *int) {
     *xPtr = 1
     fmt.Println(*xPtr)
+}
+
+func swap_number(num1 *int, num2 *int) {
+    temp := *num1
+    *num1 = *num2
+    *num2 = temp
 }
---
+++ go_struct.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+import "math"
+
+// type: introduces a new type
+// struct: defining a struct type
+type Circle struct {
+    x, y, r float64
+}
+
+type Rectangle struct {
+    x1, x2, y1, y2 float64
+}
+
+
+func main() {
+    c := Circle{x: 0, y: 0, r: 5}
+    fmt.Println(c.x, c.y, c.r)
+    fmt.Println(circleArea(c))
+
+
+    c1 := new(Circle)
+    fmt.Println(c1.x, c1.y, c1.r)
+
+    c2 := Circle{0, 0, 100}
+    circle_area := c2.area()
+    fmt.Println(circle_area)
+
+    rect := Rectangle{1, 2, 10, 100}
+    fmt.Println(rect.area())
+
+}
+
+func circleArea(c Circle) float64 {
+    return math.Pi * c.r * c.r
+}
+
+// Circle method
+func (c *Circle) area() float64 {
+    return math.Pi * c.r * c.r
+}
+
+func distance(x1, x2, y1, y2 float64) float64 {
+    a := (x2 - x1)
+    b := (y2 - y1)
+    return math.Sqrt(a * a + b * b)
+}
+
+func (rect *Rectangle) area() float64 {
+    l := distance(rect.x1, rect.y1, rect.x1, rect.y2)
+    w := distance(rect.x1, rect.y1, rect.x2, rect.y1)
+
+    return l * w
+}

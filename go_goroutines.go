26bad55c05cc01ff7570fe4d6c70aff03393902c
zhiruchen <zhiruchen1992@foxmail.com>
2016-12-18 16:32:08 +0800

    go channel

--- go_goroutines.go
+++ go_goroutines.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+     "fmt"
+     "time"
+     "math/rand"
+)
 
 func f(n int) {
     for i := 0; i <= n; i++ {
@@ -8,10 +12,18 @@ func f(n int) {
     }
 }
 
+func f1(n int) {
+    for i := 0; i < n; i++ {
+        fmt.Println(n, ":", i)
+        amt := time.Duration(rand.Intn(250))
+        time.Sleep(time.Millisecond * amt)
+    }
+}
+
 
 func main() {
     for i := 0; i < 10; i++{
-        go f(i)
+        go f1(i)
     }
     var input string
     fmt.Scanln(&input)

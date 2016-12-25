afac829151f376b821b55bbd5a54f663ba4fd907
zhiruchen <zhiruchen1992@foxmail.com>
2016-12-25 11:39:50 +0800

    update chat app

--- chat/main.go
+++ chat/main.go
@@ -6,6 +6,9 @@ import (
 	"sync"
 	"text/template"
 	"path/filepath"
+	"flag"
+	"trace"
+	"os"
 )
 
 type templateHandler struct {
@@ -18,18 +21,22 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application!")
+	flag.Parse()
 	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 
 	go r.run()  //
 
 	// 开启 web server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("starting the webserver on: ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe", err)
 	}
 
--- chat/room.go
+++ chat/room.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gorilla/websocket"
 	"net/http"
 	"log"
+	"trace"
 )
 
 const (
@@ -16,6 +17,7 @@ type room struct {
 	join    chan *client     // 希望加入房间的客户端
 	leave   chan *client     // 希望离开房间的客户端
 	clients map[*client]bool // 保持房间中所有客户端
+	tracer trace.Tracer      // 用来接受追踪房间的活动信息
 }
 
 // 创建一个新房间
@@ -33,18 +35,21 @@ func (r *room) run() {
 		select {
 		case client := <-r.join:
 			r.clients[client] = true
+			r.tracer.Trace("New client joined.")
 		case client := <-r.leave:
 			delete(r.clients, client)
 			close(client.send)
+			r.tracer.Trace("client leave.")
 		case msg := <-r.forward:
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
-				// send msg
+					r.tracer.Trace("-- msg send to client.")
 				default:
 				// 发送失败
 					delete(r.clients, client)
 					close(client.send)
+					r.tracer.Trace(" -- failed to send, clean up client.")
 				}
 			}
 		}

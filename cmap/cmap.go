17d7b6c083aa813a6fc43052dfda96bd7e976a7a
zhiruchen <zhiruchen1992@foxmail.com>
2017-08-10 08:56:41 +0800

    concurrent map

---
+++ cmap/cmap.go
@@ -0,0 +1,10 @@
+package cmap
+
+// ConcurrentMap concurrent goroutine safe map
+type ConcurrentMap interface {
+	Concurrency() int
+	Put(key string, element interface{}) (bool, error)
+	Get(key string) interface{}
+	Delete(key string) bool
+	Len() uint64
+}
